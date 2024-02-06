68d61d5cf779b3996e5689329c09bf98ac4765ce
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-06 13:06:54 +0530

    custom colors

--- gomato/src/App.js
+++ gomato/src/App.js
@@ -1,7 +1,8 @@
 function App() {
   return (
-    "hello"
+    <div>asd</div>
   );
 }
+
 
 export default App;
--- gomato/tailwind.config.js
+++ gomato/tailwind.config.js
@@ -3,6 +3,11 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
     extend: {},
+    colors: {
+      yellowish: "#F7B614",
+      blackish: "#282932",
+      ghee: "#F2EFE6"
+    }
   },
   plugins: [],
 }
