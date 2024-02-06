7d379e3733754fe41c33799ddd1e86f16f6e7151
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-06 13:40:03 +0530

    structure

--- gomato/src/App.js
+++ gomato/src/App.js
@@ -1,9 +1,10 @@
+import Body from "./components/Body";
 import Header from "./components/Header";
 
 function App() {
   return (
     <div>
-      <Header/>
+      <Body />
     </div>
   );
 }
---
+++ gomato/src/components/Hero.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Hero = () => {
+    return (
+        <div>
+            <div>
+                <div>
+                    {/* left box */}
+                    <h1>Express </h1>
+                    <h1>Home Delivery</h1>
+                    <p>Curabitur imperdiet varius lacus, id placerat purus vulputate non. Fusce in felis vel arcu maximus placerat eu ut arcu. Ut nunc ex, gravida vel porttitor et, pretium ac sapien.</p>
+                    <button>Read More</button>
+                </div>
+                <div>
+                    {/* right box */}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Hero
