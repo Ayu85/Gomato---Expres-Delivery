536b96f74e12a4b8ff2ac4bd14f2b30f5e0e3ec8
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-06 13:31:51 +0530

    header

--- gomato/src/components/Header.js
+++ gomato/src/components/Header.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import logo from "../assets/logo.png"
+import { PiPhoneCallFill } from "react-icons/pi";
+import { FaUserAlt } from "react-icons/fa";
+import { IoCart } from "react-icons/io5";
+import { MdNavigateNext } from "react-icons/md";
+
 const Header = () => {
     return (
-        <div className='bg-blackish py-6 px-3 flex justify-evenly'>
+        <div className='bg-blackish py-6 px-5 flex justify-between items-center text-lg'>
             <div>
                 {/* logo box */}
                 <img src={logo} alt='logo' />
@@ -10,15 +15,25 @@ const Header = () => {
             <div>
                 {/* menus box */}
                 <ul className='flex list-none text-white gap-4 '>
-                    <li>Home</li>
-                    <li>About us</li>
-                    <li>Catalog</li>
-                    <li>Blog</li>
-                    <li>Contacts</li>
+                    <li className='flex items-center '>Home<MdNavigateNext />
+                    </li>
+                    <li className='flex items-center '>About us<MdNavigateNext />
+                    </li>
+                    <li className='flex items-center '>Catalog<MdNavigateNext />
+                    </li>
+                    <li className='flex items-center '>Blog<MdNavigateNext />
+                    </li>
+                    <li className='flex items-center '>Contacts<MdNavigateNext />
+                    </li>
                 </ul>
             </div>
-            <div>
-                <div>+91-9005676379</div>
+            <div className='flex items-center gap-9 text-white'>
+                <div className='flex items-center gap-2 text-yellowish font-semibold'><PiPhoneCallFill className='text-white' />
+                    +91-9005676379</div>
+                <div><FaUserAlt />
+                </div>
+                <div><IoCart />
+                </div>
             </div>
 
         </div>
