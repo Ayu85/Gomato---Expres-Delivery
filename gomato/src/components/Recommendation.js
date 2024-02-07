b2cb3c8b74533c5cbf9e648b251b675b4c951b91
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-07 20:18:09 +0530

    cards made

--- gomato/src/components/Recommendation.js
+++ gomato/src/components/Recommendation.js
@@ -11,7 +11,7 @@ import rest5 from "../assets/recommendation/rest_05.jpg"
 
 const Recommendation = () => {
     return (
-        <div className='h-[110vh] px-5 flex gap-5'>
+        <div className='h-[140vh] px-5 flex gap-5'>
             {/* main div */}
             <div className='w-[45%] cursor-pointer box-border  overflow-hidden rounded-2xl relative'>
                 {/* left div */}
@@ -25,21 +25,49 @@ const Recommendation = () => {
                 <div className='absolute bottom-3 right-5  bg-yellowish px-1 py-1 rounded-full '><FaLongArrowAltRight className='text-3xl w-full hover:translate-x-5 transition-all' />
                 </div>
             </div>
-            <div className='w-[55%] '>
+            <div className='w-[55%] flex flex-wrap gap-3 '>
                 {/* right div */}
                 <div className=' h-[70vh] w-96 overflow-hidden relative'>
                     <img src={rest2} alt='logo' className='w-96 hover:scale-110 transition-all ' />
                     <div className=' w-full flex absolute bottom-0 flex-col gap-4  bg-ghee rounded-b-2xl pl-7 py-2'>
-                        <div className='text-2xl text-blackish font-bold'>Burger House</div>
+                        <div className='text-2xl text-blackish font-bold'>Restaurant Monco</div>
                         <h1 className='flex items-center gap-3  text-gray'><FaCalendarAlt className='text-yellowish' />
                             Monday - Saturday</h1>
                         <h1 className='flex items-center gap-3  text-gray'><IoIosTime className='text-yellowish text-xl' />
                             09:00 - 18:00 </h1>
                     </div>
                 </div>
-                <div></div>
-                <div></div>
-                <div></div>
+                <div className=' h-[70vh] w-96 overflow-hidden relative'>
+                    <img src={rest3} alt='logo' className='w-96 hover:scale-110 transition-all ' />
+                    <div className=' w-full flex absolute bottom-0 flex-col gap-4  bg-ghee rounded-b-2xl pl-7 py-2'>
+                        <div className='text-2xl text-blackish font-bold'>Ribs and Beer</div>
+                        <h1 className='flex items-center gap-3  text-gray'><FaCalendarAlt className='text-yellowish' />
+                            Monday - Saturday</h1>
+                        <h1 className='flex items-center gap-3  text-gray'><IoIosTime className='text-yellowish text-xl' />
+                            09:00 - 18:00 </h1>
+                    </div>
+                </div>
+                <div className=' h-[70vh] w-96 overflow-hidden relative'>
+                    <img src={rest4} alt='logo' className='w-96 hover:scale-110 transition-all ' />
+                    <div className=' w-full flex absolute bottom-0 flex-col gap-4  bg-ghee rounded-b-2xl pl-7 py-2'>
+                        <div className='text-2xl text-blackish font-bold'>Shusisteria</div>
+                        <h1 className='flex items-center gap-3  text-gray'><FaCalendarAlt className='text-yellowish' />
+                            Monday - Saturday</h1>
+                        <h1 className='flex items-center gap-3  text-gray'><IoIosTime className='text-yellowish text-xl' />
+                            09:00 - 18:00 </h1>
+                    </div>
+                </div>
+                <div className=' h-[70vh] w-96 overflow-hidden relative'>
+                    <img src={rest5} alt='logo' className='w-96 hover:scale-110 transition-all ' />
+                    <div className=' w-full flex absolute bottom-0 flex-col gap-4  bg-ghee rounded-b-2xl pl-7 py-2'>
+                        <div className='text-2xl text-blackish font-bold'>Happy Grill Cafe</div>
+                        <h1 className='flex items-center gap-3  text-gray'><FaCalendarAlt className='text-yellowish' />
+                            Monday - Saturday</h1>
+                        <h1 className='flex items-center gap-3  text-gray'><IoIosTime className='text-yellowish text-xl' />
+                            09:00 - 18:00 </h1>
+                    </div>
+                </div>
+
             </div>
         </div>
     )
