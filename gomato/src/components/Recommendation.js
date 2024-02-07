d8bd25723b1ce164ad487a5a0159a709aa9f4031
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-07 19:07:59 +0530

    recommendation

---
+++ gomato/src/components/Recommendation.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Recommendation = () => {
+    return (
+        <div>
+            {/* main div */}
+            <div>
+                {/* left div */}
+            </div>
+            <div>
+                {/* right div */}
+            </div>
+        </div>
+    )
+}
+
+export default Recommendation
