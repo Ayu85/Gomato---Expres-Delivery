97116165d89751c567bdd09a4dc80b0fe5895f57
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-07 12:38:10 +0530

    food buttons

--- gomato/src/components/Foodbuttons.js
+++ gomato/src/components/Foodbuttons.js
@@ -1,12 +1,24 @@
 import React from 'react'
+import { LiaHamburgerSolid } from "react-icons/lia";
+import { LiaPizzaSliceSolid } from "react-icons/lia";
+import { PiBowlFoodLight } from "react-icons/pi";
+import { TbMeat } from "react-icons/tb";
 
 const Foodbuttons = () => {
   return (
-    <div>
-      <button>Fast Food</button>
-      <button>Pizza</button>
-      <button>Asian Food</button>
-      <button>Raw Meat</button>
+    <div className='flex items-center justify-between px-8 mt-5 gap-8'>
+      <button className='bg-buttoncolor w-96  py-8
+       rounded-[25px] flex text-blackish gap-7 justify-evenly items-center text-xl font-[Roboto] pr-16 pl-5'><LiaHamburgerSolid className='bg-yellowish w-20 h-20 px-2 text-blackish font-light text-2xl rounded-full' />
+        Fastfood</button>
+      <button className='bg-buttoncolor w-96  py-8
+       rounded-[25px] flex text-blackish gap-7 justify-evenly items-center text-xl font-[Roboto] pr-16 pl-5'>< LiaPizzaSliceSolid className='bg-yellowish w-20 h-20 px-2 text-blackish font-light text-2xl rounded-full' />
+        Pizza</button>
+      <button className='bg-buttoncolor w-96  py-8
+       rounded-[25px] flex text-blackish gap-7 justify-evenly items-center text-xl font-[Roboto] pr-16 pl-5'><PiBowlFoodLight className='bg-yellowish w-20 h-20 px-2 text-blackish font-light text-2xl rounded-full' />
+        Asian Food</button>
+      <button className='bg-buttoncolor w-96  py-8
+       rounded-[25px] flex text-blackish gap-7 justify-evenly items-center text-xl font-[Roboto] pr-16 pl-5'><TbMeat className='bg-yellowish w-20 h-20 px-2 text-blackish font-light text-2xl rounded-full' />
+        Raw Meat</button>
     </div>
   )
 }
