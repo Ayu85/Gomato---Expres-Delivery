d9f5c0d28431b2d6fe8acaa084886d197b249d88
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-07 22:51:59 +0530

    pizza ad

--- gomato/src/components/PizzaAd.js
+++ gomato/src/components/PizzaAd.js
@@ -1,11 +1,22 @@
 import React from 'react'
 
 const PizzaAd = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div className='bg-yellowish'>
+            <div>
+                {/* left div */}
+                <h1>Always</h1>
+                <h1>the Hottest</h1>
+                <h1>Pizza</h1>
+                <p>Curabitur imperdiet varius lacus, id placerat purus vulputate non. Fusce in felis vel arcu maximus placerat eu ut arcu. Ut nunc ex, gravida vel porttitor et, pretium ac sapien.</p>
+                <button className={`bg_slider2 px-5 sm:mt-10 lg:mt-2 mt-10 py-5 w-60 text-lg font-semibold text-blackish bg-yellowish rounded-full `}>Read More</button>
+
+            </div>
+            <div>
+                {/* right div */}
+            </div>
+        </div>
+    )
 }
 
 export default PizzaAd
