24dc999998456cad2fad365efd84f085c6a4d833
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-07 19:06:18 +0530

    starting recommendation section

--- gomato/src/components/Quickpick Section/Foodcard.js
+++ gomato/src/components/Quickpick Section/Foodcard.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Foodcard = ({ name, price, about, img }) => {
     return (
-        <div className='flex flex-col items-center justify-center gap-3 w-80 px-3 text-center bg-white'>
+        <div className='flex flex-col items-center justify-center gap-3 w-80 px-3 text-center cursor-pointer bg-white'>
             <div className='w-80'>
-                <img src={img} alt='logo' />
+                <img src={img} alt='logo' className='hover:scale-110 transition-all'/>
             </div>
             <h1 className='text-lg font-semibold text-blackish'>{name}</h1>
             <h1 className='text-gray line-clamp-2 w-60'>{about}</h1>
