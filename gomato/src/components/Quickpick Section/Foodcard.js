71545b40aa715f717788ec6226cb9f34a7135e34
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-07 13:48:01 +0530

    food card

--- gomato/src/components/Quickpick Section/Foodcard.js
+++ gomato/src/components/Quickpick Section/Foodcard.js
@@ -1,11 +1,16 @@
 import React from 'react'
 
-const Foodcard = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+const Foodcard = ({ name, price, about, img }) => {
+    return (
+        <div className='flex flex-col items-center justify-center gap-3'>
+            <div>
+                <img src={img} alt='logo' />
+            </div>
+            <h1>{name}</h1>
+            <h1>{about}</h1>
+            <h1>{price}</h1>
+        </div>
+    )
 }
 
 export default Foodcard
--- gomato/src/components/Quickpick Section/Quickpick.js
+++ gomato/src/components/Quickpick Section/Quickpick.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Headings from './Headings'
+import Foodcontainer from './Foodcontainer'
 
 const Quickpick = () => {
     return (
         <div className='flex justify-center mt-5'>
             <Headings />
+            <Foodcontainer/>
         </div>
     )
 }
