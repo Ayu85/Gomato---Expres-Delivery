509a546030c18ba5866ef18228eb950df2507145
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-07 14:40:04 +0530

    redux working perfectly

--- gomato/src/components/Quickpick Section/Headings.js
+++ gomato/src/components/Quickpick Section/Headings.js
@@ -9,7 +9,7 @@ const Headings = () => {
         <div className='flex flex-col justify-center gap-3 items-center py-4'>
             <h1 className='text-yellowish font-semibold text-lg'>Quick Picks</h1>
             <h1 className='text-4xl text-blackish font-semibold'>Popular Goods</h1>
-            <div className='flex justify-center items-center gap-6 text-[1.03rem] font-semibold'>
+            <div className='flex justify-center items-center gap-6 text-[1.03rem] font-semibold pt-4'>
                 <button className='border-b border-blackish border-dashed pb-1 hover:text-yellowish' onClick={() => {
                     dispatch(changeItem('burger'))
                 }}>Fast Food</button>
