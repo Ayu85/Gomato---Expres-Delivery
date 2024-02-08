75e8cf8f7f445ed1ba924ab1a316d02d456a3e24
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-08 22:29:18 +0530

    store set up

--- gomato/src/components/Medical Supplies/Medicalsupplies.js
+++ gomato/src/components/Medical Supplies/Medicalsupplies.js
@@ -3,9 +3,13 @@ import Header from "../Header"
 import Heromedical from './Heromedical'
 import Products from './Products'
 import { FaSliders } from "react-icons/fa6";
+import { toogleBar } from '../../redux/slices/Productslide';
+import { useDispatch, useSelector } from 'react-redux';
 
 const Medicalsupplies = () => {
   const [title, setTitle] = useState('Medical Supplies - GoMoto')
+  const barStatus = useSelector(store => store.productSlider.value)
+  const dispatch = useDispatch()
   useEffect(() => {
     document.title = title
   }, [title])
@@ -14,7 +18,9 @@ const Medicalsupplies = () => {
       <Header />
       <Heromedical />
       <Products />
-      <div className='absolute left-0 z-[999] top-[50%] bg-yellowish rounded-r-full px-3 py-2 cursor-pointer'><FaSliders className='text-2xl' />
+      <div onClick={() => {
+        dispatch(toogleBar())
+      }} className='absolute left-0 z-[999] top-[50%] bg-yellowish rounded-r-full px-3 py-2 cursor-pointer'><FaSliders className='text-2xl' />
       </div>
     </div>
   )
