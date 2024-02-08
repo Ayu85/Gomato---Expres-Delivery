4c706dc34595f7d0ed4276bd2142a1e191c1eeb7
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-08 22:31:34 +0530

    bar working

--- gomato/src/components/Medical Supplies/Products.js
+++ gomato/src/components/Medical Supplies/Products.js
@@ -10,6 +10,8 @@ import { RxCross1, RxHamburgerMenu } from "react-icons/rx";
 
 const Products = () => {
   const currentIndex = useSelector(store => store.currentProductIndex.value)
+  const barStatus = useSelector(store => store.productSlider.value)
+
   return (
     <div className='flex px-6 relative '>
       {/* category section */}
@@ -62,10 +64,10 @@ const Products = () => {
       {/* ***************************************/}
       {/* side bar opening and closing */}
       {/* ***************************************/}
-      {/* <div className='absolute z-50 bg-white left-0 w-96 h-full'>
-          <RxCross1 className='text-4xl ml-[50%] -translate-x-[50%] mt-6 -mb-4'/>
+      <div className={`absolute z-50 bg-white left-0 w-96 h-full ${!barStatus ? "-translate-x-[500px] transition-all duration-300" : "translate-x-0 transition-all duration-300"}`}>
+        <RxCross1 className='text-4xl ml-[50%] -translate-x-[50%] mt-6 -mb-4' />
         <div className='bg-[#F2EFE6] w-full mt-10 py-8 px-5 flex  relative items-center justify-center  rounded-2xl '>
-        
+
           <input type='text' placeholder='Enter Product Name' className='pl-5 z-50 px-8 py-4 rounded-full text-blackish placeholder:text-gray w-[600px]' />
           <div className='bg-yellowish absolute z-50 right-6  px-4 py-4 rounded-full text-blackish '><IoSearch className='' />
           </div>
@@ -96,7 +98,7 @@ const Products = () => {
             </ul>
           </ul>
         </div>
-      </div> */}
+      </div>
     </div>
   )
 }
