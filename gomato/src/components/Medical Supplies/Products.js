be8d5c3e08b13cc77e4f9b801b801f28692323b2
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-08 15:06:09 +0530

    prod page

--- gomato/src/components/Medical Supplies/Products.js
+++ gomato/src/components/Medical Supplies/Products.js
@@ -3,9 +3,9 @@ import React from 'react'
 const Products = () => {
   return (
     <div>
-      <div className='bg-ghee mt-10 py-10 flex items-center justify-center'>
+      <div className='bg-ghee mt-10 py-7 flex items-center justify-center w-[70%] rounded-2xl ml-[50%] -translate-x-[50%]'>
         {/* searchbar */}
-        <input type='text' placeholder='Enter Product Name' className='pl-5 px-8 py-3 rounded-full text-blackish placeholder:text-gray w-[600px]'/>
+        <input type='text' placeholder='Enter Product Name' className='pl-5 px-8 py-5 rounded-full text-blackish placeholder:text-gray w-[600px]' />
       </div>
       <div>
         {/* products */}
