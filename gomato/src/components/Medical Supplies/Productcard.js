089f820d9da8a641e75ddfb9aa5ddc9bb692cac7
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-08 16:14:34 +0530

    fucked the whole page by making it 100% working

--- gomato/src/components/Medical Supplies/Productcard.js
+++ gomato/src/components/Medical Supplies/Productcard.js
@@ -1,13 +1,13 @@
 import React from 'react'
-
-const Productcard = ({logo,product_name,price}) => {
+import { motion } from 'framer-motion'
+const Productcard = ({ logo, product_name, price }) => {
     return (
-        <div className='w-80 flex flex-col items-center justify-center gap-2 cursor-pointer hover:shadow-lg hover:scale-105 transition-all shadow-gray border-ghee border rounded-xl py-5 px-5'>
+        <motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ duration: 1 }} className='w-80  flex flex-col items-center justify-center gap-2 cursor-pointer hover:shadow-lg hover:scale-105 transition-all shadow-gray border-ghee border rounded-xl py-5 px-5'>
             <div><img src={logo} alt='logo' className='w-72' /></div>
             <div className='text-blackish text-lg font-semibold text-center'>{product_name}</div>
             <div className='text-lg font-semibold text-gray'>💲{price}</div>
             <button className='bg_slider px-5 py-2 rounded-full'>Add To Cart</button>
-        </div>
+        </motion.div>
     )
 }
 
