dfba64d653ea5eb6a64e16223739d4a7e030d7fa
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-08 22:18:41 +0530

    opening and closing bar

--- gomato/src/components/Medical Supplies/Products.js
+++ gomato/src/components/Medical Supplies/Products.js
@@ -6,11 +6,12 @@ import wave from '../../assets/overlay-waves-white.png'
 import { MdNavigateNext } from "react-icons/md";
 import { IoSearch } from "react-icons/io5";
 import Pagebuttons from './Pagebuttons'
+import { RxCross1, RxHamburgerMenu } from "react-icons/rx";
 
 const Products = () => {
   const currentIndex = useSelector(store => store.currentProductIndex.value)
   return (
-    <div className='flex px-6 '>
+    <div className='flex px-6 relative '>
       {/* category section */}
       <div className='hidden lg:flex w-[30%]  flex-col gap-3'>
         <div className='bg-[#F2EFE6] w-full mt-10 py-8 px-5 flex relative items-center justify-center  rounded-2xl '>
@@ -47,8 +48,8 @@ const Products = () => {
         </div>
       </div>
       {/* product section */}
-      <div className='w-[70%] flex flex-col justify-center items-center gap-4'>
-        <div className='flex  justify-center items-center pt-10 gap-5 flex-wrap '>
+      <div className=' flex flex-col justify-center items-center gap-4'>
+        <div className='flex   justify-center items-center pt-10 gap-5 flex-wrap '>
           {/* products */}
           {
             medicalSupplies[currentIndex].map((items) => {
@@ -58,7 +59,44 @@ const Products = () => {
         </div>
         <Pagebuttons />
       </div>
-
+      {/* ***************************************/}
+      {/* side bar opening and closing */}
+      {/* ***************************************/}
+      <div className='absolute z-50 bg-white left-0 w-96 h-full'>
+          <RxCross1 className='text-4xl ml-[50%] -translate-x-[50%] mt-6 -mb-4'/>
+        <div className='bg-[#F2EFE6] w-full mt-10 py-8 px-5 flex  relative items-center justify-center  rounded-2xl '>
+          {/* searchbar */}
+          <input type='text' placeholder='Enter Product Name' className='pl-5 z-50 px-8 py-4 rounded-full text-blackish placeholder:text-gray w-[600px]' />
+          <div className='bg-yellowish absolute z-50 right-6  px-4 py-4 rounded-full text-blackish '><IoSearch className='' />
+          </div>
+        </div>
+        <div className=' border border-[#d2d1d1] border-dashed px-5 rounded-2xl py-5 mt-5'>
+          <h1 className='text-xl text-blackish font-semibold border-b border-[#d2d1d1] border-dashed pb-5'>🚀PRODUCT CATEGORIES</h1>
+          <ul className='flex flex-col gap-3 py-5'>
+            <li className='font-semibold text-blackish flex items-center gap-1'><MdNavigateNext className='text-yellowish font-bold text-lg' />Asian Food</li>
+            <li className='font-semibold text-blackish flex items-center gap-1'><MdNavigateNext className='text-yellowish font-bold text-lg' />Fast Food</li>
+            <li className='font-semibold text-blackish flex items-center gap-1'><MdNavigateNext className='text-yellowish font-bold text-lg' />Hot Pizza</li>
+            <ul className='ml-8 text-sm -mt-2'>
+              <li className='font-semibold text-blackish my-2 flex items-center gap-1'><MdNavigateNext className='text-yellowish font-bold text-lg' />Cheesy</li>
+              <li className='font-semibold text-blackish my-2 flex items-center gap-1'><MdNavigateNext className='text-yellowish font-bold text-lg' />Fish and Seafood</li>
+              <li className='font-semibold text-blackish my-2 flex items-center gap-1'><MdNavigateNext className='text-yellowish font-bold text-lg' />Italian</li>
+              <li className='font-semibold text-blackish my-2 flex items-center gap-1'><MdNavigateNext className='text-yellowish font-bold text-lg' />Vegeterian</li>
+            </ul>
+            <li className='font-semibold text-blackish flex items-center gap-1'><MdNavigateNext className='text-yellowish font-bold text-lg' />Water Delivery</li>
+            <li className='font-semibold text-blackish flex items-center gap-1'><MdNavigateNext className='text-yellowish font-bold text-lg' />Raw Meat</li>
+            <ul className='font-semibold  text-blackish flex items-center gap-1'><MdNavigateNext className='text-yellowish font-bold text-lg' />Medical Supplies</ul>
+            <ul className='ml-8 text-sm -mt-2'>
+              <li className='font-semibold text-blackish my-2 flex items-center gap-1'><MdNavigateNext className='text-yellowish font-bold text-lg' />Antiseptic</li>
+              <li className='font-semibold text-blackish my-2 flex items-center gap-1'><MdNavigateNext className='text-yellowish font-bold text-lg' />Face Masks</li>
+              <li className='font-semibold text-blackish my-2 flex items-center gap-1'><MdNavigateNext className='text-yellowish font-bold text-lg' />Equipement</li>
+              <li className='font-semibold text-blackish my-2 flex items-center gap-1'><MdNavigateNext className='text-yellowish font-bold text-lg' />Gloves</li>
+              <li className='font-semibold text-blackish my-2 flex items-center gap-1'><MdNavigateNext className='text-yellowish font-bold text-lg' />Pills</li>
+              <li className='font-semibold text-blackish my-2 flex items-center gap-1'><MdNavigateNext className='text-yellowish font-bold text-lg' />Latex Gloves</li>
+              <li className='font-semibold text-blackish my-2 flex items-center gap-1'><MdNavigateNext className='text-yellowish font-bold text-lg' />Vitamins</li>
+            </ul>
+          </ul>
+        </div>
+      </div>
     </div>
   )
 }
