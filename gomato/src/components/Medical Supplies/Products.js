1f0770a287b568cb16cc109373e050bcd387c09a
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-08 16:11:30 +0530

    redux store working smoothly

--- gomato/src/components/Medical Supplies/Products.js
+++ gomato/src/components/Medical Supplies/Products.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react'
 import { medicalSupplies } from "../../utils/__mocks__"
 import Productcard from './Productcard'
+import { useSelector } from 'react-redux'
 const Products = () => {
-  const [currentIndex, setIndex] = useState(0)
+  // const [currentIndex, setIndex] = useState(0)
+  const currentIndex = useSelector(store => store.currentProductIndex.value)
   return (
     <div>
       <div className='bg-ghee mt-10 py-7 px-5 flex items-center justify-center w-[70%] rounded-2xl ml-[50%] -translate-x-[50%]'>
