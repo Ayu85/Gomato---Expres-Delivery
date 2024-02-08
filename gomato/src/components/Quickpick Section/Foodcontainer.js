e62242df2e1fdb5eee65179d945e879759b35c51
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-08 11:43:53 +0530

    framer animation

--- gomato/src/components/Quickpick Section/Foodcontainer.js
+++ gomato/src/components/Quickpick Section/Foodcontainer.js
@@ -17,7 +17,7 @@ import meat2 from "../../assets/food items/meat_02-1024x1024.jpg"
 import meat3 from "../../assets/food items/meat_03-1024x1024.jpg"
 import meat4 from "../../assets/food items/meat_04-1024x1024.jpg"
 import { useDispatch, useSelector } from 'react-redux'
-
+import { motion, useScroll } from 'framer-motion'
 const Foodcontainer = () => {
     const dispatch = useDispatch();
     const currentItem = useSelector(store => store.currentitem.item)
@@ -25,32 +25,32 @@ const Foodcontainer = () => {
     return (
         <div >
             {/* burger food */}
-            {currentItem === 'burger' && <div className='pt-5 flex flex-wrap  justify-center lg:justify-between gap-5 px-7 sm:flex-wrap sm:justify-center md:justify-center lg:flex-nowrap'><Foodcard name={'Cheeseburger with Salad'} about={'A burger fully loaded with cheese and salad for better health , enjoy the taste of cheese and health together'} img={burger1} price={19.20} />
+            {currentItem === 'burger' && <motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ duration: 1 }} className='pt-5 flex flex-wrap  justify-center lg:justify-between gap-5 px-7 sm:flex-wrap sm:justify-center md:justify-center lg:flex-nowrap'><Foodcard name={'Cheeseburger with Salad'} about={'A burger fully loaded with cheese and salad for better health , enjoy the taste of cheese and health together'} img={burger1} price={19.20} />
                 <Foodcard name={'Black Gamburger with Fishcake'} about={'A burger fully loaded with cheese and salad for better health , enjoy the taste of cheese and health together'} img={burger2} price={19.20} />
                 <Foodcard name={'Classic Bacon Hamburger'} about={'A burger fully loaded with cheese and salad for better health , enjoy the taste of cheese and health together'} img={burger3} price={19.20} />
                 <Foodcard name={'Royal Cheeseburger with Bacon'} about={'A burger fully loaded with cheese and salad for better health , enjoy the taste of cheese and health together'} img={burger4} price={19.20} />
-            </div>}
+            </motion.div>}
             {/* pizza food */}
-            {currentItem === 'pizza' && <div className='pt-5 flex flex-wrap  justify-center lg:justify-between gap-5 px-7 sm:flex-wrap sm:justify-center md:justify-center lg:flex-nowrap'>
+            {currentItem === 'pizza' && <motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ duration: 1 }} className='pt-5 flex flex-wrap  justify-center lg:justify-between gap-5 px-7 sm:flex-wrap sm:justify-center md:justify-center lg:flex-nowrap'>
                 <Foodcard name={'Shrimp and Olive Pizza'} about={'A burger fully loaded with cheese and salad for better health , enjoy the taste of cheese and health together'} img={pizza1} price={16.20} />
                 <Foodcard name={'Seafood Pizza'} about={'A burger fully loaded with cheese and salad for better health , enjoy the taste of cheese and health together'} img={pizza2} price={23.20} />
                 <Foodcard name={'Vegetarian Pizza'} about={'A burger fully loaded with cheese and salad for better health , enjoy the taste of cheese and health together'} img={pizza3} price={19.20} />
                 <Foodcard name={'Pizza With Mushrooms'} about={'A burger fully loaded with cheese and salad for better health , enjoy the taste of cheese and health together'} img={pizza4} price={17.20} />
-            </div>}
+            </motion.div>}
             {/* asian food */}
-            {currentItem === 'asian' && < div className='pt-5 flex flex-wrap  justify-center lg:justify-between gap-5 px-7 sm:flex-wrap sm:justify-center md:justify-center lg:flex-nowrap'>
+            {currentItem === 'asian' && < motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ duration: 1 }} className='pt-5 flex flex-wrap  justify-center lg:justify-between gap-5 px-7 sm:flex-wrap sm:justify-center md:justify-center lg:flex-nowrap'>
                 <Foodcard name={'Red Fish Roll'} about={'A burger fully loaded with cheese and salad for better health , enjoy the taste of cheese and health together'} img={asian1} price={23.20} />
                 <Foodcard name={'Seafood '} about={'A burger fully loaded with cheese and salad for better health , enjoy the taste of cheese and health together'} img={asian2} price={33.20} />
                 <Foodcard name={'Octopus Roll'} about={'A burger fully loaded with cheese and salad for better health , enjoy the taste of cheese and health together'} img={asian3} price={30.20} />
                 <Foodcard name={'Shrimp Roll'} about={'A burger fully loaded with cheese and salad for better health , enjoy the taste of cheese and health together'} img={asian4} price={25.20} />
-            </div>}
+            </motion.div>}
             {/* raw meat food */}
-            {currentItem === 'meat' && <div className='pt-5 flex flex-wrap  justify-center lg:justify-between gap-5 px-7 sm:flex-wrap sm:justify-center md:justify-center lg:flex-nowrap'>
+            {currentItem === 'meat' && <motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ duration: 1 }} className='pt-5 flex flex-wrap  justify-center lg:justify-between gap-5 px-7 sm:flex-wrap sm:justify-center md:justify-center lg:flex-nowrap'>
                 <Foodcard name={'Whole Chicken'} about={'A burger fully loaded with cheese and salad for better health , enjoy the taste of cheese and health together'} img={meat1} price={23.20} />
                 <Foodcard name={'Boiled Pork With Spices'} about={'A burger fully loaded with cheese and salad for better health , enjoy the taste of cheese and health together'} img={meat2} price={33.20} />
                 <Foodcard name={'Chicken Breast'} about={'A burger fully loaded with cheese and salad for better health , enjoy the taste of cheese and health together'} img={meat3} price={30.20} />
                 <Foodcard name={'Herb Chicken'} about={'A burger fully loaded with cheese and salad for better health , enjoy the taste of cheese and health together'} img={meat4} price={25.20} />
-            </div>}
+            </motion.div>}
         </div >
     )
 }
