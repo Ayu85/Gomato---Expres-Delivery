7b8ead0b19280515f054b268c44c59d50108d53c
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-08 12:34:53 +0530

    framer motion op

--- gomato/src/components/Recommendation.js
+++ gomato/src/components/Recommendation.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { FaCalendarAlt } from "react-icons/fa";
 import { IoIosTime } from "react-icons/io";
 import { FaLongArrowAltRight } from "react-icons/fa";
-
+import { motion } from 'framer-motion'
 import rest1 from "../assets/recommendation/rest_01.jpg"
 import rest2 from "../assets/recommendation/rest_02.jpg"
 import rest3 from "../assets/recommendation/rest_03.jpg"
@@ -17,7 +17,7 @@ const Recommendation = () => {
             <h1 className='text-blackish font-semibold text-4xl -mt-4'>Cafes and Restautants</h1>
             {/* main container box with 6 restaurants */}
             <div className='flex items-center justify-center gap-10 flex-wrap'>
-                <div className='w-96 h-[30rem] rounded-2xl bg-blackish overflow-hidden relative'>
+                <motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ duration: 1 }} className='w-96 h-[30rem] rounded-2xl bg-blackish overflow-hidden relative'>
                     {/* 1 restaurant */}
                     <div ><img src={rest1} alt='logo' className='hover:scale-110 transition-all  cursor-pointer' /></div>
                     <div className='absolute bottom-0 bg-ghee w-full flex flex-col gap-3 pl-6 py-3'>
@@ -25,8 +25,8 @@ const Recommendation = () => {
                         <h1 className='flex items-center gap-2 '><FaCalendarAlt className='text-yellowish text-lg' />Monday - Saturday</h1>
                         <h1 className='flex items-center gap-2 '><IoIosTime FaCalendarAlt className='text-yellowish text-xl' />9:00 AM - 11:00 PM </h1>
                     </div>
-                </div>
-                <div className='w-96 h-[30rem] rounded-2xl bg-blackish overflow-hidden relative'>
+                </motion.div>
+                <motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ duration: 1 }} className='w-96 h-[30rem] rounded-2xl bg-blackish overflow-hidden relative'>
                     {/* 1 restaurant */}
                     <div ><img src={rest2} alt='logo' className='hover:scale-110 transition-all  cursor-pointer' /></div>
                     <div className='absolute bottom-0 bg-ghee w-full flex flex-col gap-3 pl-6 py-3'>
@@ -34,8 +34,8 @@ const Recommendation = () => {
                         <h1 className='flex items-center gap-2 '><FaCalendarAlt className='text-yellowish text-lg' />Monday - Saturday</h1>
                         <h1 className='flex items-center gap-2 '><IoIosTime FaCalendarAlt className='text-yellowish text-xl' />9:00 AM - 11:00 PM </h1>
                     </div>
-                </div>
-                <div className='w-96 h-[30rem] rounded-2xl bg-blackish overflow-hidden relative'>
+                </motion.div>
+                <motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ duration: 1 }} className='w-96 h-[30rem] rounded-2xl bg-blackish overflow-hidden relative'>
                     {/* 1 restaurant */}
                     <div ><img src={rest3} alt='logo' className='hover:scale-110 transition-all  cursor-pointer' /></div>
                     <div className='absolute bottom-0 bg-ghee w-full flex flex-col gap-3 pl-6 py-3'>
@@ -43,8 +43,8 @@ const Recommendation = () => {
                         <h1 className='flex items-center gap-2 '><FaCalendarAlt className='text-yellowish text-lg' />Monday - Saturday</h1>
                         <h1 className='flex items-center gap-2 '><IoIosTime FaCalendarAlt className='text-yellowish text-xl' />9:00 AM - 11:00 PM </h1>
                     </div>
-                </div>
-                <div className='w-96 h-[30rem] rounded-2xl bg-blackish overflow-hidden relative'>
+                </motion.div>
+                <motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ duration: 1 }} className='w-96 h-[30rem] rounded-2xl bg-blackish overflow-hidden relative'>
                     {/* 1 restaurant */}
                     <div ><img src={rest4} alt='logo' className='hover:scale-110 transition-all  cursor-pointer' /></div>
                     <div className='absolute bottom-0 bg-ghee w-full flex flex-col gap-3 pl-6 py-3'>
@@ -52,8 +52,8 @@ const Recommendation = () => {
                         <h1 className='flex items-center gap-2 '><FaCalendarAlt className='text-yellowish text-lg' />Monday - Saturday</h1>
                         <h1 className='flex items-center gap-2 '><IoIosTime FaCalendarAlt className='text-yellowish text-xl' />9:00 AM - 11:00 PM </h1>
                     </div>
-                </div>
-                <div className='w-96 h-[30rem] rounded-2xl bg-blackish overflow-hidden relative'>
+                </motion.div>
+                <motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ duration: 1 }} className='w-96 h-[30rem] rounded-2xl bg-blackish overflow-hidden relative'>
                     {/* 1 restaurant */}
                     <div ><img src={rest5} alt='logo' className='hover:scale-110 transition-all  cursor-pointer' /></div>
                     <div className='absolute bottom-0 bg-ghee w-full flex flex-col gap-3 pl-6 py-3'>
@@ -61,8 +61,8 @@ const Recommendation = () => {
                         <h1 className='flex items-center gap-2 '><FaCalendarAlt className='text-yellowish text-lg' />Monday - Saturday</h1>
                         <h1 className='flex items-center gap-2 '><IoIosTime FaCalendarAlt className='text-yellowish text-xl' />9:00 AM - 11:00 PM </h1>
                     </div>
-                </div>
-                <div className='w-96 h-[30rem] rounded-2xl bg-blackish overflow-hidden relative'>
+                </motion.div>
+                <motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ duration: 1 }} className='w-96 h-[30rem] rounded-2xl bg-blackish overflow-hidden relative'>
                     {/* 1 restaurant */}
                     <div ><img src={rest1} alt='logo' className='hover:scale-110 transition-all  cursor-pointer' /></div>
                     <div className='absolute bottom-0 bg-ghee w-full flex flex-col gap-3 pl-6 py-3'>
@@ -70,7 +70,7 @@ const Recommendation = () => {
                         <h1 className='flex items-center gap-2 '><FaCalendarAlt className='text-yellowish text-lg' />Monday - Saturday</h1>
                         <h1 className='flex items-center gap-2 '><IoIosTime FaCalendarAlt className='text-yellowish text-xl' />9:00 AM - 11:00 PM </h1>
                     </div>
-                </div>
+                </motion.div>
             </div>
 
         </div>
