e4009464b84e93a9675615f0bc3781c53c39cfc9
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-08 13:53:31 +0530

    header reused

--- gomato/src/components/Medicalsupplies.js
+++ gomato/src/components/Medicalsupplies.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-
+import Header from "../components/Header"
 const Medicalsupplies = () => {
   const [title, setTitle] = useState('Medical Supplies - GoMoto')
   useEffect(() => {
@@ -7,7 +7,7 @@ const Medicalsupplies = () => {
   }, [title])
   return (
     <div>
-        
+        <Header/>
     </div>
   )
 }
