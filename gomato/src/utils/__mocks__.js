af9228170eba87c9329cf4485c0563e6546ade07
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-08 13:00:55 +0530

    mock data for items

---
+++ gomato/src/utils/__mocks__.js
@@ -0,0 +1,64 @@
+const medicalSupplies = [
+    [
+        {
+            logo: 'http://gomoto.like-themes.com/wp-content/uploads/2019/06/med_store_01-600x600.jpg',
+            product_name: 'Vitamin Course',
+            price: 19.25
+        },
+        {
+            logo: 'http://gomoto.like-themes.com/wp-content/uploads/2019/06/med_store_02-600x600.jpg        ',
+            product_name: 'Dry Cough Tablets',
+            price: 20.5
+        },
+        {
+            logo: 'http://gomoto.like-themes.com/wp-content/uploads/2019/06/med_store_03-600x600.jpg        ',
+            product_name: 'Pocket Infrared Thermometer',
+            price: 22.23
+        },
+        {
+            logo: 'http://gomoto.like-themes.com/wp-content/uploads/2019/06/med_store_04-600x600.jpg        ',
+            product_name: ' Digital Thermometer',
+            price: 15.25
+        },
+        {
+            logo: 'http://gomoto.like-themes.com/wp-content/uploads/2019/06/med_store_05-600x600.jpg        ',
+            product_name: 'Antiseptic Spray(200ml)',
+            price: 21.25
+        }, {
+            logo: 'http://gomoto.like-themes.com/wp-content/uploads/2019/06/med_store_06-600x600.jpg        ',
+            product_name: 'Blood Pressure Monitor',
+            price: 24.25
+        }
+    ],
+    [
+        {
+            logo: 'http://gomoto.like-themes.com/wp-content/uploads/2019/06/med_store_07.jpg            ',
+            product_name: 'Cough Syrup(200ml)',
+            price: 16.25
+        },
+        {
+            logo: 'http://gomoto.like-themes.com/wp-content/uploads/2019/06/med_store_08.jpg            ',
+            product_name: 'Medical Mask',
+            price: 16.39
+        },
+        {
+            logo: 'http://gomoto.like-themes.com/wp-content/uploads/2019/06/med_store_09.jpg            ',
+            product_name: 'Sthethoscope',
+            price: 18.99
+        },
+        {
+            logo: 'http://gomoto.like-themes.com/wp-content/uploads/2019/06/med_store_10.jpg            ',
+            product_name: 'Antibacterial Gel(200ml)',
+            price: 14.25
+        },
+        {
+            logo: 'http://gomoto.like-themes.com/wp-content/uploads/2019/06/med_store_12.jpg            ',
+            product_name: 'Dental Kit',
+            price: 22.25
+        }, {
+            logo: 'http://gomoto.like-themes.com/wp-content/uploads/2019/06/med_store_13.jpg            ',
+            product_name: 'Instrument Container ',
+            price: 26.25
+        }
+    ]
+]
