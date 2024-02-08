b3460027fd3a05e6842900cb1c168559135bd2de
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-08 16:09:50 +0530

    dispatching action

--- gomato/src/components/Medical Supplies/Pagebuttons.js
+++ gomato/src/components/Medical Supplies/Pagebuttons.js
@@ -1,12 +1,23 @@
 import React from 'react'
-
+import { toogleIndex } from '../../redux/slices/Currentproductindex'
+import { UseDispatch, useDispatch } from 'react-redux'
 const Pagebuttons = () => {
+    const dispatch = useDispatch();
+
     return (
         <div className='flex gap-3 items-center justify-center  py-10'>
-            <button className='hover:bg-blackish hover:text-white transition-all duration-200 bg-ghee text-blackish text-lg px-6 py-4 mx-2 font-semibold rounded-full'>1</button>
-            <button className='hover:bg-blackish hover:text-white transition-all duration-200 bg-ghee text-blackish text-lg px-6 py-4 mx-2 font-semibold rounded-full'>2</button>
-            <button className='hover:bg-blackish hover:text-white transition-all duration-200 bg-ghee text-blackish text-lg px-6 py-4 mx-2 font-semibold rounded-full'>3</button>
-            <button className=' hover:bg-blackish hover:text-white transition-all duration-200 text-blackish text-lg bg-ghee font-semibold px-6 py-4 mx-2 rounded-full'>4</button>
+            <button onClick={() => {
+                dispatch(toogleIndex(0))
+            }} className='hover:bg-blackish hover:text-white transition-all duration-200 bg-ghee text-blackish text-lg px-6 py-4 mx-2 font-semibold rounded-full'>1</button>
+            <button onClick={() => {
+                dispatch(toogleIndex(1))
+            }} className='hover:bg-blackish hover:text-white transition-all duration-200 bg-ghee text-blackish text-lg px-6 py-4 mx-2 font-semibold rounded-full'>2</button>
+            <button onClick={() => {
+                dispatch(toogleIndex(2))
+            }} className='hover:bg-blackish hover:text-white transition-all duration-200 bg-ghee text-blackish text-lg px-6 py-4 mx-2 font-semibold rounded-full'>3</button>
+            <button onClick={() => {
+                dispatch(toogleIndex(3))
+            }} className=' hover:bg-blackish hover:text-white transition-all duration-200 text-blackish text-lg bg-ghee font-semibold px-6 py-4 mx-2 rounded-full'>4</button>
         </div>
     )
 }
