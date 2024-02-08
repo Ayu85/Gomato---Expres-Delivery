5ebbaa3e0eb227e55980e6ecfa775928482d26dd
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-08 22:21:44 +0530

    provided to store

--- gomato/src/redux/store.js
+++ gomato/src/redux/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit'
 import Hamburgerslice from './slices/Hamburgerslice';
 import Currentfooditem from './slices/Currentfooditem';
 import Currentproductindex from './slices/Currentproductindex'
+import Productslide from './slices/Productslide';
 const store = configureStore({
     reducer: {
         Hamburger: Hamburgerslice,
         currentitem: Currentfooditem,
-        currentProductIndex: Currentproductindex
+        currentProductIndex: Currentproductindex,
+        productSlider: Productslide
     }
 })
 export default store;
