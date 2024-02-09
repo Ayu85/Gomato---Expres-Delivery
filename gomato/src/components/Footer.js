ecb5e5c196947bda2a332153ff09526fec3fc6d3
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-09 13:42:12 +0530

    footer done

--- gomato/src/components/Footer.js
+++ gomato/src/components/Footer.js
@@ -8,7 +8,7 @@ const Footer = () => {
             <div className='flex py-10 px-5 relative'>
                 <div><img src={footerbg} alt='logo' className='rounded-l-xl' /></div>
                 <div className='bg-yellowish w-[530px] rounded-r-xl'></div>
-                <div className='px-5 py-5 cursor-pointer bg-white rounded-full absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2'> <div className='bg-ghee px-3 py-3 rounded-full '><IoCallSharp className='text-4xl text-yellowish' />
+                <div className='hover:bg-blackish transition-all duration-200 px-4 py-4 cursor-pointer bg-white rounded-full absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2'> <div className='bg-ghee px-3 py-3 rounded-full '><IoCallSharp className='text-4xl text-yellowish' />
                 </div></div>
             </div>
 
