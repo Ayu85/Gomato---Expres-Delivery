0158853b4327a7de854f3c98fd6b570879d7ebca
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-09 12:40:44 +0530

    login page

--- gomato/src/components/Login.js
+++ gomato/src/components/Login.js
@@ -1,12 +1,17 @@
 import React from 'react'
+import { FcGoogle } from "react-icons/fc";
 
 const Login = () => {
     return (
-        <div>
+        <div className='flex flex-col gap-2'>
             <h1>Log In</h1>
+            <div>
+                < FcGoogle />
+
+            </div>
             <form>
-                <input type='email' placeholder='Enter Email'/>
-                <input type='password' placeholder='Enter Password'/>
+                <input type='email' placeholder='Enter Email' />
+                <input type='password' placeholder='Enter Password' />
 
             </form>
         </div>
