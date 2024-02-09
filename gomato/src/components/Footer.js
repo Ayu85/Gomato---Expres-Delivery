d8d7148cad421ad06a2911f05f3be3c04296eeeb
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-09 13:26:37 +0530

    footer bg

--- gomato/src/components/Footer.js
+++ gomato/src/components/Footer.js
@@ -2,9 +2,10 @@ import React from 'react'
 import footerbg from '../assets/footer-banner.jpg'
 const Footer = () => {
     return (
-        <div className='bg-blackish'>
-            <div>
-                <div><img src={footerbg} alt='logo' /></div>
+        <div className='bg-blackish flex justify-center items-center'>
+            <div className='flex py-10'>
+                <div><img src={footerbg} alt='logo' className='rounded-l-xl' /></div>
+                <div className='bg-yellowish w-[550px] rounded-r-xl'></div>
             </div>
 
         </div>
