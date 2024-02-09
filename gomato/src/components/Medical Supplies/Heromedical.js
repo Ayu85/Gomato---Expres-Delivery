62adfd5ab8570b376e0aff4f849a804bcd95dbeb
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-09 19:33:27 +0530

    more responsive

--- gomato/src/components/Medical Supplies/Heromedical.js
+++ gomato/src/components/Medical Supplies/Heromedical.js
@@ -3,10 +3,10 @@ import { FaBriefcaseMedical } from "react-icons/fa";
 
 const Heromedical = ({ name, bg }) => {
   return (
-    <div className='h-[90vh] relative  '>
+    <div className='lg:h-[90vh] md:h-[90vh] h-[50vh] relative w-screen '>
       <div className='z-10 absolute w-full'>
         <img src={bg}
-          alt='logo' className='w-full h-[100vh] brightness-50 bg-fixed' />
+          alt='logo' className='w-fulllg:h-[90vh] md:h-[90vh] h-[50vh] w-screen brightness-50 bg-fixed' />
       </div>
       <div className='absolute z-50 text-white font-bold text-5xl left-1/2 
       -translate-x-1/2 top-1/2 -translate-y-1/2 flex items-center gap-6 text-center'>{name}
