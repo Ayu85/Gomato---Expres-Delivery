3a809d6b9faaf9ccb54fec8215b62a948e2a5035
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-09 12:53:00 +0530

    login

--- gomato/src/components/Login.js
+++ gomato/src/components/Login.js
@@ -3,16 +3,16 @@ import { FcGoogle } from "react-icons/fc";
 
 const Login = () => {
     return (
-        <div className='flex flex-col gap-2'>
-            <h1>Log In</h1>
+        <div className='flex flex-col gap-2 mt-20 w-[500px]  ml-[50%] -translate-x-[50%] items-center py-20 bg-blackish text-white justify-center'>
+            <h1 className='text-4xl font-semibold '>LOG IN</h1>
             <div>
                 < FcGoogle />
 
             </div>
-            <form>
-                <input type='email' placeholder='Enter Email' />
-                <input type='password' placeholder='Enter Password' />
-
+            <form className='flex flex-col gap-3'>
+                <input type='email' placeholder='Enter Email' className='w-96 py-3 rounded-xl pl-3 text-gray outline-none' />
+                <input type='password' placeholder='Enter Password' className='w-96 py-3 rounded-xl pl-3 text-gray outline-none' />
+                <button>Log In</button>
             </form>
         </div>
     )
--- gomato/src/components/User.js
+++ gomato/src/components/User.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import Header from '../components/Header'
 import Heromedical from '../components/Medical Supplies/Heromedical'
+import Login from './Login'
 const User = () => {
   const [title] = useState('My Account-GoMoto Delivery')
   useEffect(() => {
@@ -10,7 +11,7 @@ const User = () => {
     <div>
       <Header />
       <Heromedical name={"MY ACCOUNT"} bg={'https://images.pexels.com/photos/4974915/pexels-photo-4974915.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'} />
-      
+      <Login/>
     </div>
   )
 }
