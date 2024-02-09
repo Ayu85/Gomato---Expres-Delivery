34b8182af64543b7197a0074a04e49645682687f
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-09 13:24:03 +0530

    making footer

---
+++ gomato/src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import footerbg from '../assets/footer-banner.jpg'
+const Footer = () => {
+    return (
+        <div className='bg-blackish'>
+            <div>
+                <div><img src={footerbg} alt='logo' /></div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Footer
