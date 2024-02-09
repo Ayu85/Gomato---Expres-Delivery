5d3b9b8584dea48675a10325c78e52343badf92c
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-09 19:49:10 +0530

    cart items displaying

--- gomato/src/components/Cart.js
+++ gomato/src/components/Cart.js
@@ -11,16 +11,16 @@ const Cart = () => {
         <div>
             <Header />
             <Heromedical name={"CART"} bg={'https://img.freepik.com/free-photo/black-friday-concept-with-cart-basket_23-2147696392.jpg?w=996&t=st=1707486962~exp=1707487562~hmac=46456e50b1e0c3bb8bd86ac5e23d31a988207edf53bc91904bede0fd721be5e8'} />
-            <div className='flex justify-center items-center'>
+            <div className='flex justify-center items-center mt-20'>
                 <div>
                     {
                         items.length !== 0 ? items.map((product) => {
-                            return <div>
-                                <div>
+                            return <div className='flex border-gray border items-center justify-center gap-5 w-[550px] '>
+                                <div className='w-[100px]'>
                                     <img src={product.logo} alt='logo' className='w-28' />
                                 </div>
-                                <div>{product.name}</div>
-                                <div>{product.price}</div>
+                                <div className='font-semibold w-[300px]'>{product.name}</div>
+                                <div className='font-semibold'>💲{product.price}</div>
                             </div>
                         }) : <h1>cart empty</h1>
                     }
