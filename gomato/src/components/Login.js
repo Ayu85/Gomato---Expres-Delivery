020019759d9db5b4466b236604082b47157fcc84
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-09 13:09:40 +0530

    responsive

--- gomato/src/components/Login.js
+++ gomato/src/components/Login.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import { FcGoogle } from "react-icons/fc";
 import { FcPhone } from "react-icons/fc";
-
+import waves from '../assets/overlay-waves-white.png'
 const Login = () => {
     return (
-        <div className='flex flex-col gap-6 rounded-xl mt-36 py-20 w-[500px]  ml-[50%] -translate-x-[50%] items-center mb-20 bg-blackish text-white justify-center'>
+        <div className='flex relative flex-col gap-6 px-10 rounded-xl mt-36 py-20 lg:w-[500px] md:w-[500px] w-[300px]  ml-[50%] -translate-x-[50%] items-center mb-20 bg-blackish text-white justify-center'>
             <h1 className='text-4xl font-semibold '>LOG IN</h1>
             <div className='flex items-center gap-3'>
                 {/* logos page */}
@@ -15,10 +15,14 @@ const Login = () => {
                     <FcPhone className='text-3xl' />
                 </div></div>
             <form className='flex flex-col gap-5'>
-                <input type='email' placeholder='Enter Email' className='w-96 py-3 rounded-xl pl-3 text-gray outline-none' />
-                <input type='password' placeholder='Enter Password' className='w-96 py-3 rounded-xl pl-3 text-gray outline-none' />
-                <button className='w-96 bg_slider2 font-semibold text-lg py-3 rounded-xl pl-3 text-blackish bg-yellowish outline-none'>Log In</button>
+                <input type='email' placeholder='Enter Email' className='lg:w-96 md:w-96 py-3 w-52 rounded-xl pl-3 text-gray outline-none' />
+                <input type='password' placeholder='Enter Password' className='lg:w-96 md:w-96 w-52 py-3 rounded-xl pl-3 text-gray outline-none' />
+                <button className='lg:w-96 md:w-96  w-52  bg_slider2 font-semibold text-lg py-3 rounded-xl pl-3 text-blackish bg-yellowish outline-none'>Log In</button>
             </form>
+            {/* overlay bg */}
+            <div className='absolute'>
+                <img src={waves} alt='logo' className='w-full h-[70vh]' />
+            </div>
         </div>
     )
 }
