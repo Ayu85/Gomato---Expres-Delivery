ceed817fe57227b5962d8d932a4055a4997db3e1
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-09 12:20:33 +0530

    user page

--- gomato/src/components/User.js
+++ gomato/src/components/User.js
@@ -1,9 +1,15 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import Header from '../components/Header'
+import Heromedical from '../components/Medical Supplies/Heromedical'
 const User = () => {
+  const [title] = useState('My Account-GoMoto Delivery')
+  useEffect(() => {
+    document.title = title
+  }, [])
   return (
     <div>
-      user
+      <Header />
+      <Heromedical name={"MY ACCOUNT"} bg={'https://images.pexels.com/photos/4974915/pexels-photo-4974915.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'} />
     </div>
   )
 }
