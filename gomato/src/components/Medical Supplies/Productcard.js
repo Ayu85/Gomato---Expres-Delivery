b611267087bafc9d3c1236c224153ec229282a60
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-09 19:18:20 +0530

    dispatched an action

--- gomato/src/components/Medical Supplies/Productcard.js
+++ gomato/src/components/Medical Supplies/Productcard.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import { motion } from 'framer-motion'
+import { useDispatch } from 'react-redux'
+import { addItem } from '../../redux/slices/Cartslice'
 const Productcard = ({ logo, product_name, price }) => {
+    const dispatch = useDispatch()
     return (
-        <motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ duration: 1 , delay:0.5 }} className='w-72  flex flex-col items-center justify-center gap-2 cursor-pointer hover:shadow-lg hover:scale-105 transition-all shadow-gray border-ghee border rounded-xl py-5 px-5'>
+        < motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ duration: 1, delay: 0.5 }} className='w-72  flex flex-col items-center justify-center gap-2 cursor-pointer hover:shadow-lg hover:scale-105 transition-all shadow-gray border-ghee border rounded-xl py-5 px-5' >
             <div><img src={logo} alt='logo' className='w-72' /></div>
             <div className='text-blackish text-lg font-semibold text-center'>{product_name}</div>
             <div className='text-lg font-semibold text-gray'>💲{price}</div>
-            <button className='bg_slider px-5 py-2 rounded-full'>Add To Cart</button>
-        </motion.div>
+            <button onClick={() => {
+                dispatch(addItem(product_name))
+            }} className='bg_slider px-5 py-2 rounded-full'>Add To Cart</button>
+        </motion.div >
     )
 }
 
