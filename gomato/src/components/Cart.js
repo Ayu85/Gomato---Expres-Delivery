a45be6d39e9123500b0ff5dd3680d1691213df7f
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-09 19:35:15 +0530

    dynamic title

--- gomato/src/components/Cart.js
+++ gomato/src/components/Cart.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Header from './Header'
 import Heromedical from '../components/Medical Supplies/Heromedical'
 const Cart = () => {
+    useEffect(() => {
+        document.title='Cart - GoMoto Delivery'
+    }, [])
     return (
         <div>
             <Header />
-            <Heromedical name={"CART"} bg={'https://img.freepik.com/free-photo/black-friday-concept-with-cart-basket_23-2147696392.jpg?w=996&t=st=1707486962~exp=1707487562~hmac=46456e50b1e0c3bb8bd86ac5e23d31a988207edf53bc91904bede0fd721be5e8'}/>
+            <Heromedical name={"CART"} bg={'https://img.freepik.com/free-photo/black-friday-concept-with-cart-basket_23-2147696392.jpg?w=996&t=st=1707486962~exp=1707487562~hmac=46456e50b1e0c3bb8bd86ac5e23d31a988207edf53bc91904bede0fd721be5e8'} />
         </div>
     )
 }
