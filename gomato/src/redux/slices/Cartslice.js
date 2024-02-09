cb064cf0fdf8d545e794acdaa83f0edd7f79e4e5
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-09 19:14:57 +0530

    cart slice created

---
+++ gomato/src/redux/slices/Cartslice.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cart = createSlice({
+    name: 'cart',
+    initialState: {
+        items: []
+    },
+    reducers: {
+        addItem: (state, action) => {
+            state.items.push(action.payload)
+        },
+        removeItem: (state) => {
+            state.items.pop()
+        },
+        clearCart: (state) => {
+            state.items = []
+        }
+
+    }
+})
+export const { addItem, removeItem, clearCart } = cart.actions;
+export default cart.reducer;
