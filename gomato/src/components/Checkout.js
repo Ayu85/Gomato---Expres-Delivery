fd555aaf4262e77600bc0a00306860630c2317e0
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-10 13:09:30 +0530

    form getting ready

--- gomato/src/components/Checkout.js
+++ gomato/src/components/Checkout.js
@@ -10,22 +10,24 @@ const Checkout = () => {
         <div>
             <Header />
             <Heromedical name={'CHECKOUT'} bg={'https://images.pexels.com/photos/6289028/pexels-photo-6289028.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'} />
-
+            <div>
+                <AddressForm />
+            </div>
         </div>
     )
 }
 const AddressForm = () => {
-    return <div>
+    return <div className='flex flex-col gap-3 w-[800px] '>
         <h1>Billing Details</h1>
-        <div className='flex '>
-            <input type='text' placeholder='Enter First Name ' />
-            <input type='text' placeholder='Enter Last Name ' />
+        <div className='flex w-full gap-3'>
+            <input type='text' placeholder='Enter First Name '  className='border border-gray py-5 w-[400px] rounded-full pl-2'/>
+            <input type='text' placeholder='Enter Last Name ' className='border border-gray py-5 w-[400px] rounded-full pl-2'/>
         </div>
 
-        <input type='text' placeholder='Enter Full Address' />
-        <input type='text' placeholder='Enter Landmark' />
-        <input type='text' placeholder='Enter Town/City' />
-        <input type='text' placeholder='Enter Pin Code' />
+        <input type='text' placeholder='Enter Full Address' className='border border-gray py-5 w-full rounded-full pl-2'/>
+        <input type='text' placeholder='Enter Landmark' className='border border-gray py-5 w-full rounded-full pl-2'/>
+        <input type='text' placeholder='Enter Town/City' className='border border-gray py-5 w-full rounded-full pl-2'/>
+        <input type='text' placeholder='Enter Pin Code' className='border border-gray py-5 w-full rounded-full pl-2'/>
 
     </div>
 }
