a50142959424309434b5bb080baf59de8f930c60
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-10 23:02:40 +0530

    user slice created

---
+++ gomato/src/redux/slices/Userslice.js
@@ -0,0 +1,15 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const user = createSlice({
+    name: 'user',
+    initialState: {
+        user: null
+    },
+    reducers: {
+        addUser: (state, action) => {
+            state.user = action.payload
+        }
+    }
+})
+export const { addUser } = user.actions
+export default user.reducer
