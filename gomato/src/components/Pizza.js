913da7e73e6470fe5df0413f0544a2ee3394a037
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-10 11:55:59 +0530

    pizza page making

---
+++ gomato/src/components/Pizza.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from './Header'
+import Heromedical from './Medical Supplies/Heromedical'
+const Pizza = () => {
+    return (
+        <div>
+            <Header />
+            <Heromedical name={'PIZZA'} />
+        </div>
+    )
+}
+
+export default Pizza
