931fcf6711b10fb4976a6a20ba101a125723611f
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-10 13:04:24 +0530

    making bill details

--- gomato/src/components/Checkout.js
+++ gomato/src/components/Checkout.js
@@ -1,14 +1,32 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Header from './Header'
 import Heromedical from './Medical Supplies/Heromedical'
 
 const Checkout = () => {
+    useEffect(() => {
+        document.title = 'Checkout- GoMoto Delivery '
+    }, [])
     return (
         <div>
             <Header />
             <Heromedical name={'CHECKOUT'} bg={'https://images.pexels.com/photos/6289028/pexels-photo-6289028.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'} />
+
         </div>
     )
 }
+const AddressForm = () => {
+    return <div>
+        <h1>Billing Details</h1>
+        <div className='flex '>
+            <input type='text' placeholder='Enter First Name ' />
+            <input type='text' placeholder='Enter Last Name ' />
+        </div>
 
+        <input type='text' placeholder='Enter Full Address' />
+        <input type='text' placeholder='Enter Landmark' />
+        <input type='text' placeholder='Enter Town/City' />
+        <input type='text' placeholder='Enter Pin Code' />
+
+    </div>
+}
 export default Checkout
