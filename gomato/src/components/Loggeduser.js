c70e4ec0db560799d70ce82c06023f9c1c7be9fd
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-10 22:59:07 +0530

    after login redirection

---
+++ gomato/src/components/Loggeduser.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './Header'
+
+const Loggeduser = () => {
+    return (
+        <div>
+            <Header />
+            <div>
+                welcome user
+            </div>
+        </div>
+    )
+}
+
+export default Loggeduser
