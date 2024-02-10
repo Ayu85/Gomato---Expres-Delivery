2580a26d0cd7e9a190f38a9f141bfd5bd4b9269b
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-10 11:30:46 +0530

    data rendered on ui

--- gomato/src/components/Waterdelivery.js
+++ gomato/src/components/Waterdelivery.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import Header from '../components/Header'
 import Heromedical from '../components/Medical Supplies/Heromedical'
+import Products from './Medical Supplies/Products'
+import { waterDelivery } from '../utils/__mocks__'
 const Waterdelivery = () => {
-  return (
-    <div>
-      <Header/>
-      <Heromedical name='WATER DELIVERY' bg='https://images.pexels.com/photos/928854/pexels-photo-928854.jpeg?cs=srgb&dl=pexels-stephan-m%C3%BCller-928854.jpg&fm=jpg'/>
-    </div>
-  )
+    return (
+        <div>
+            <Header />
+            <Heromedical name='WATER DELIVERY' bg='https://images.pexels.com/photos/928854/pexels-photo-928854.jpeg?cs=srgb&dl=pexels-stephan-m%C3%BCller-928854.jpg&fm=jpg' />
+            <Products products={waterDelivery} />
+        </div>
+    )
 }
 
 export default Waterdelivery
