45a1b6e807d15644ee84a77590cf5d9be21adabc
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-10 11:25:37 +0530

    water delivery

--- gomato/src/components/Waterdelivery.js
+++ gomato/src/components/Waterdelivery.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Header from '../components/Header'
+import Heromedical from '../components/Medical Supplies/Heromedical'
 const Waterdelivery = () => {
   return (
     <div>
       <Header/>
-      
+      <Heromedical name='WATER DELIVERY' bg='https://images.pexels.com/photos/416528/pexels-photo-416528.jpeg?auto=compress&cs=tinysrgb&w=600'/>
     </div>
   )
 }
