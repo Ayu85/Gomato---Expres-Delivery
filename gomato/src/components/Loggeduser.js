c89c6bc456c36d815cd687d29b0de66dcacb5e95
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2024-02-11 13:34:50 +0530

    login and logout refactoring

--- gomato/src/components/Loggeduser.js
+++ gomato/src/components/Loggeduser.js
@@ -6,6 +6,9 @@ import { useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { addUser } from '../redux/slices/Userslice';
 import { motion } from 'framer-motion'
+import welcomelogo from '../assets/welcome.png'
+import lockedlogo from '../assets/locked.png'
+import userlogo from '../assets/userlogo.png'
 const Loggeduser = () => {
     const [showData, setData] = useState(false)
     useEffect(() => {
@@ -50,8 +53,10 @@ const Userdetails = () => {
     const user = useSelector(store => store.userSlice.user)
     return <div className='flex items-center justify-center bg-blackish h-screen'>
         <div className='bg-[#00000039]  text-white px-24 rounded-lg h-[500px] flex flex-col gap-4 items-center py-5'>
-            <h1 className='text-yellowish text-3xl '>Welcome Buddy</h1>
-            <h1>Email: {user.currentuser || "Please Log In"}</h1>
+            {/* <h1 className='text-yellowish text-3xl '>Welcome Buddy</h1> */}
+            <img src={!user ? lockedlogo : welcomelogo} alt='logo' className='w-72' />
+            <h1 className='text-xl font-semibold'>Email: {user?.currentuser || "Please Log In"}</h1>
+            <button>Take me to Login Page</button>
         </div>
     </div>
 }
